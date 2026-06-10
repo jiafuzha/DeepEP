9059d806bce97dae7f7dd9cb06f131b905f36fd6
Jiafu Zhang <jiafu.zhang@intel.com>
2026-06-10 20:05:26 +0000

    move further with BF16 topk passed
    Signed-off-by: Jiafu Zhang <jiafu.zhang@intel.com>

--- tests/docker-2node/run.sh
+++ tests/docker-2node/run.sh
@@ -163,6 +163,7 @@ run_test() {
     docker exec \
         -e ISHMEM_DEBUG="${ISHMEM_DEBUG:-0}" \
         -e DEEP_EP_DBG_DISPATCH="${DEEP_EP_DBG_DISPATCH:-}" \
+        -e DEEP_EP_DBG_COMBINE="${DEEP_EP_DBG_COMBINE:-}" \
         deepep-node0 \
         bash -lc "
             source /opt/intel/oneapi/setvars.sh --force >/dev/null 2>&1
@@ -186,6 +187,7 @@ run_test() {
                 -genv FI_PROVIDER tcp \
                 -genv ISHMEM_DEBUG \"\${ISHMEM_DEBUG:-0}\" \
                 -genv DEEP_EP_DBG_DISPATCH \"\${DEEP_EP_DBG_DISPATCH:-}\" \
+                -genv DEEP_EP_DBG_COMBINE \"\${DEEP_EP_DBG_COMBINE:-}\" \
                 -genv DEEP_EP_NVL_RANKS $NUM_PROCESSES \
                 -genv DEEP_EP_NVL_BYTES $DEEP_EP_NVL_BYTES \
                 -genv DEEP_EP_RDMA_BYTES $DEEP_EP_RDMA_BYTES \
